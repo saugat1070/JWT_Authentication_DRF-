2a3bfe3b614dd6d148597886bd0fc974b1ab23b8
saugat1070 <saugatgiri1070@gmail.com>
2025-02-12 23:58:38 +0545

    updating admin panel

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Authentication.models import Student
 
-# Register your models here.
+# # Register your models here.
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ['id_number','name','class_room']
+
+# admin.site.register(Student,StudentAdmin)
+admin.site.register(Student)
+
