7ecc782c22d91f6d9f6f56642c79a14e3ddfa795
saugat1070 <saugatgiri1070@gmail.com>
2025-02-13 15:13:33 +0545

    modified admin.py file

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from Authentication.models import Student
 
 # # Register your models here.
-# class StudentAdmin(admin.ModelAdmin):
-#     list_display = ['id_number','name','class_room']
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id_number','name','class_room']
 
-# admin.site.register(Student,StudentAdmin)
-admin.site.register(Student)
+admin.site.register(Student,StudentAdmin)
+
 
